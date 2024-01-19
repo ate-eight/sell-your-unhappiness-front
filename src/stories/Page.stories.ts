6da3196c352fa690fbe9e32a38c623ef3bba482d
bbung <tjd123123@naver.com>
2024-01-19 19:39:45 +0900

    feat: color palette add

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -4,7 +4,7 @@ import { expect, userEvent, within } from '@storybook/test';
 import { Page } from './Page';
 
 const meta = {
-    title: 'Example/Page',
+    title: 'Layout/Page',
     component: Page,
     parameters: {
         // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
