66a5ae4deb3bf03ed09eb0ff5e7bec09cd1fd5d5
SeongEun Park <77668478+bbung95@users.noreply.github.com>
2024-01-22 16:33:13 +0900

    Storybook 배포 설정 수정 (#2)
    * feat: color palette add
    
    * fix: package conflict fix
    
    - vite 4.4.5 downgrade
    - chromatic add
    
    * feat: chromatic deploy ci/cd add
    
    * fix: chromatic.yml fix
    
    * feat: emotion theme add and palette fix

---
+++ .storybook/preview.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+
+import theme from '../src/styles/theme';
+
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+    decorators: [
+        (Story) => {
+            return (
+                <div style={{ minWidth: '475px' }}>
+                    <ThemeProvider theme={theme}>
+                        <Story />
+                    </ThemeProvider>
+                </div>
+            );
+        },
+    ],
+};
+
+export default preview;
---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+export const color = {
+    purple: {
+        100: '#B09EF8',
+        800: '#4700B9',
+    },
+    navy: {
+        400: '#4E4994',
+        900: '#10004F',
+    },
+    gray: {
+        100: '#FFFFFF',
+        200: '#F9F9F9',
+        300: '#E4E4E4',
+        400: '#B6B6B6',
+        700: '#5C5C5C',
+        800: '#2F2F2F',
+        900: '#1A1A1A',
+    },
+};
+
+const theme = { color };
+
+export default theme;
