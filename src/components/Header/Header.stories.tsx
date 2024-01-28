c8b43a5263f34d1c94dc7d53adac886ceb90bfbb
bbung <tjd123123@naver.com>
2024-01-29 00:36:55 +0900

    feat: header component add

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Header from './Header';
+
+const meta = {
+    title: 'Components/Header',
+    component: Header,
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {},
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
