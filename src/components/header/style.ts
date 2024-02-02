11657721bdfb264a36d58d2c48ce1d52e780c3c6
hyewon_park2 <hyewon_park2@tmax.co.kr>
2024-02-02 11:26:00 +0900

    test: header storybook 생성

---
+++ src/components/header/style.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.div`
+    width: 390px;
+    height: 56px;
+    background-color: ${(props) => props.theme.color.navy[900]};
+`;
