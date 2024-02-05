7a4835b3d8d21470e57bfe6154ed5cfa5a0ec289
hyewon_park2 <hyewon_park2@tmax.co.kr>
2024-02-05 19:30:26 +0900

    test : font-display:swap 설정 추가

--- src/components/text/style.ts
+++ src/components/text/style.ts
@@ -2,42 +2,46 @@ import styled from '@emotion/styled';
 
 export const H1 = styled.h1<{ color?: string }>`
     color: ${(props) => (props.color ? props.color : props.theme.color.gray[900])};
-    font-family: ${(props) => props.theme.typography.type.ExtraBold}, sans-serif;
+    font-family: ${(props) => props.theme.typography.type.ExtraBold};
     font-size: ${(props) => props.theme.typography.size.l2}px;
     font-style: normal;
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[600]};
     line-height: ${(props) => props.theme.typography.lineHeight.l2}px;
+    font-display: swap;
 `;
 
 export const H2 = styled.h2<{ color?: string }>`
     color: ${(props) => (props.color ? props.color : props.theme.color.gray[900])};
-    font-family: ${(props) => props.theme.typography.type.SemiBold}, sans-serif;
+    font-family: ${(props) => props.theme.typography.type.SemiBold}
     font-size: ${(props) => props.theme.typography.size.l1}px;
     font-style: normal;
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[600]};
     line-height: ${(props) => props.theme.typography.lineHeight.l1}px;
+    font-display: swap;
 `;
 
 export const H4_ENG = styled.h3<{ color?: string }>`
     color: ${(props) => (props.color ? props.color : props.theme.color.gray[900])};
-    font-family: ${(props) => props.theme.typography.type.SemiBold}, sans-serif;
+    font-family: ${(props) => props.theme.typography.type.SemiBold};
     font-size: ${(props) => props.theme.typography.size.m2}px;
     font-style: normal;
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[600]};
     line-height: ${(props) => props.theme.typography.lineHeight.m2}px;
+    font-display: swap;
 `;
 
 export const H4_KR = styled.h4<{ color?: string }>`
     color: ${(props) => (props.color ? props.color : props.theme.color.gray[900])};
-    font-family: ${(props) => props.theme.typography.type.Medium}, sans-serif;
+    font-family: ${(props) => props.theme.typography.type.Medium};
     font-size: ${(props) => props.theme.typography.size.m2}px;
     font-style: normal;
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[500]};
     line-height: ${(props) => props.theme.typography.lineHeight.m2}px;
+    font-display: swap;
 `;
 
 export const BodyM = styled.p<{ color?: string }>`
@@ -48,6 +52,7 @@ export const BodyM = styled.p<{ color?: string }>`
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[400]};
     line-height: ${(props) => props.theme.typography.lineHeight.m1}px;
+    font-display: swap;
 `;
 
 export const BodyS = styled.p<{ color?: string }>`
@@ -58,6 +63,7 @@ export const BodyS = styled.p<{ color?: string }>`
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[400]};
     line-height: ${(props) => props.theme.typography.lineHeight.s3}px;
+    font-display: swap;
 `;
 
 export const CaptionM = styled.p<{ color?: string }>`
@@ -68,6 +74,7 @@ export const CaptionM = styled.p<{ color?: string }>`
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[400]};
     line-height: ${(props) => props.theme.typography.lineHeight.s2}px;
+    font-display: swap;
 `;
 
 export const CaptionS = styled.p<{ color?: string }>`
@@ -78,4 +85,5 @@ export const CaptionS = styled.p<{ color?: string }>`
     margin: 0;
     font-weight: ${(props) => props.theme.typography.weight[400]};
     line-height: ${(props) => props.theme.typography.lineHeight.s1}px;
+    font-display: swap;
 `;
