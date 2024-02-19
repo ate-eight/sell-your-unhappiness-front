cd7d74f9c79ba20db24509b609b6f721873255fb
bbung <tjd123123@naver.com>
2024-02-19 19:49:52 +0900

    feat: google login handle add

--- src/components/features/signin/SignIn.tsx
+++ src/components/features/signin/SignIn.tsx
@@ -1,9 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 import ContentTag from '@/components/common/text/ContentTag';
 import Heading from '@/components/common/text/Heading';
 
 import * as S from './style';
 
 const SignIn = () => {
+    const navigate = useNavigate();
+
+    /**
+     * Google Login Logic
+     */
+    const googleLogin = () => {
+        alert('google Login');
+        navigate('/main');
+    };
+
     return (
         <S.Container>
             <S.Wrapper>
@@ -17,7 +29,7 @@ const SignIn = () => {
                         as='M'
                     />
                 </S.InfoBox>
-                <S.GoogleButton>구글로 로그인</S.GoogleButton>
+                <S.GoogleButton onClick={googleLogin}>구글로 로그인</S.GoogleButton>
             </S.Wrapper>
         </S.Container>
     );
