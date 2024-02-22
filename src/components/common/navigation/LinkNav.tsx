7940cf25f113156aebd0a51779ed3df4d6e0be63
heywon0909 <hw7319@naver.com>
2024-02-22 23:42:40 +0900

    fix : query string 으로 변환

--- src/components/common/navigation/LinkNav.tsx
+++ src/components/common/navigation/LinkNav.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
 
 import Heading from '../text/Heading';
@@ -14,7 +15,18 @@ const LinkNav = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const { search } = useLocation();
-    const onMoveSearch = (type: string) => () => {
+
+    useEffect(() => {
+        const settingQuerystring = () => {
+            if (!searchParams.get('sort')) {
+                searchParams.set('sort', 'all');
+                setSearchParams(searchParams);
+            }
+        };
+        settingQuerystring();
+    }, []);
+
+    const handleMoveSearch = (type: string) => () => {
         searchParams.set('sort', type);
         setSearchParams(searchParams);
     };
@@ -22,7 +34,7 @@ const LinkNav = () => {
     return (
         <S.LinkContainer>
             {queryArr.map((query) => (
-                <S.HeaderLink key={query.name} onClick={onMoveSearch(query.sortType)}>
+                <S.HeaderLink key={query.name} onClick={handleMoveSearch(query.sortType)}>
                     <Heading
                         as='h2'
                         text={query.name}
