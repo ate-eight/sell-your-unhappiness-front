f0594cdfb3aec8a5d2b98ef4515cf8daeaec1e50
hyewon_park2 <hyewon_park2@tmax.co.kr>
2024-02-23 14:17:25 +0900

    style : mypage back Button 생성

--- src/components/locator/Locator.tsx
+++ src/components/locator/Locator.tsx
@@ -1,13 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 import Icon from '../common/icon/Icon';
 import SubTitle from '../common/text/SubTitle';
 import * as S from './style';
 const Locator = () => {
+    const navigate = useNavigate();
+    const handleBack = () => navigate(-1);
     return (
         <S.LocatorContainer>
             <S.LocatorTitle>
                 <SubTitle text='내 활동' lan='KR' />
             </S.LocatorTitle>
-            <S.LocatorCloseButton>
+            <S.LocatorCloseButton onClick={handleBack}>
                 <Icon name='i-close' />
             </S.LocatorCloseButton>
         </S.LocatorContainer>
