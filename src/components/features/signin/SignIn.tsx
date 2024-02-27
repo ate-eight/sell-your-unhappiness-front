16eb3c6b6a60cc4a5d8c60f1d1bb8826bd6933e4
bbung <tjd123123@naver.com>
2024-02-27 16:09:29 +0900

    test: login-page test add and google icon add

--- src/components/features/signin/SignIn.tsx
+++ src/components/features/signin/SignIn.tsx
@@ -11,16 +11,19 @@ const SignIn = () => {
         <S.Container>
             <S.Wrapper>
                 <S.LogoImageWrapper>
-                    <img src='https://via.placeholder.com/236x236' alt='logo' />
+                    <img src='https://via.placeholder.com/236x236' alt='로고' />
                 </S.LogoImageWrapper>
                 <S.InfoBox>
                     <Heading text='Sell Your Unhappiness' as='h2' />
                     <ContentTag
-                        text='이곳에 블행이나 아쉬운 마음을 털어놓고 위안을 받으세요!'
+                        text='이곳에 불행이나 아쉬운 마음을 털어놓고 위안을 받으세요!'
                         as='M'
                     />
                 </S.InfoBox>
-                <S.GoogleButton onClick={googleLogin}>구글로 로그인</S.GoogleButton>
+                <S.GoogleButton onClick={googleLogin}>
+                    <img src='i-google.svg' alt='구글 로고' />
+                    구글로 로그인
+                </S.GoogleButton>
             </S.Wrapper>
         </S.Container>
     );
--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -8,9 +8,10 @@ const PrivateRouter = ({ isLogin }: Props) => {
     return isLogin ? (
         <Outlet />
     ) : (
-        <div>
-            로그인이 필요한 페이지입니다. <Link to={'/'}>로그인 페이지</Link>
-        </div>
+        <>
+            <h1>로그인이 필요한 페이지입니다.</h1>
+            <Link to={'/'}>로그인 페이지</Link>
+        </>
     );
 };
 
