bebff21bb764a667866e5a64cd87659721089297
LAPTOP-JN3UFLM6\user <alstjr9438@naver.com>
2024-02-27 14:48:58 +0900

    feat: locationbar component 추가

--- src/components/common/layout/ContentLayout.tsx
+++ src/components/common/layout/ContentLayout.tsx
@@ -1,23 +1,31 @@
 import { Outlet } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
-import * as S from './style';
+import LocationBar from '../locationBar/LocationBar';
 
 const ContentLayout = () => {
     const location = useLocation();
     let headerText = '';
+    let asType = '';
 
-    // 현재 경로에 따라 headerText 설정
+    // 현재 경로에 따라 headerText, headerType 설정
     switch (location.pathname) {
         case '/contents/create':
             headerText = '글쓰기';
+            asType = 'subtitle';
             break;
         default:
             headerText = '글읽기';
+            asType = 'back';
     }
     return (
         <>
-            <S.ContentsHeader>{headerText}</S.ContentsHeader>
+            <LocationBar
+                location={headerText}
+                as={asType}
+                onRegister={function (): void {}}
+                onBack={function (): void {}}
+            />
             <Outlet />
         </>
     );
--- src/components/common/locationBar/LocationBar.tsx
+++ src/components/common/locationBar/LocationBar.tsx
@@ -7,33 +7,57 @@ interface Props {
     location?: string;
     onRegister: () => void;
     onBack: () => void;
+    as?: string;
 }
-const Header = ({ location, onRegister, onBack }: Props) => {
-    return (
-        <S.LocationContainer>
-            <S.ButtonContainer>
+
+const Header = (props: Props) => {
+    const { location, onRegister, onBack, as } = props;
+    if (as === 'all') {
+        return (
+            <S.LocationContainer width={'390px'}>
+                <S.ButtonContainer>
+                    {/* 뒤로 가기 */}
+                    <S.Button onClick={onBack}>
+                        <ContentTag text='취소' color='#fff' as='M' />
+                    </S.Button>
+                    <S.Button cursor={'unset'}>
+                        <Icon name='i-back' color='#fff' />
+                    </S.Button>
+                    <S.HeaderLink to='/'>
+                        <Icon name='i-home' color='#fff' />
+                    </S.HeaderLink>
+                </S.ButtonContainer>
+                <SubTitle text={location as string} lan='ENG' color='#fff' />
+                <S.ButtonContainer>
+                    <S.HeaderLink to='/user'>
+                        <Icon name='i-user' color='#fff' />
+                    </S.HeaderLink>
+                    {/* 등록 버튼 */}
+                    <S.Button onClick={onRegister}>
+                        <ContentTag text='등록' color='#fff' as='M' />
+                    </S.Button>
+                </S.ButtonContainer>
+            </S.LocationContainer>
+        );
+    } else if (as === 'back') {
+        return (
+            <S.LocationContainer width={'100%'}>
                 {/* 뒤로 가기 */}
                 <S.Button onClick={onBack}>
-                    <ContentTag text='취소' color='#fff' as='M' />
-                </S.Button>
-                <S.Button cursor={'unset'}>
                     <Icon name='i-back' color='#fff' />
                 </S.Button>
+                <SubTitle text={location as string} lan='ENG' color='#fff' />
                 <S.HeaderLink to='/'>
                     <Icon name='i-home' color='#fff' />
                 </S.HeaderLink>
-            </S.ButtonContainer>
-            <SubTitle text={location as string} lan='ENG' color='#fff' />
-            <S.ButtonContainer>
-                <S.HeaderLink to='/user'>
-                    <Icon name='i-user' color='#fff' />
-                </S.HeaderLink>
-                {/* 등록 버튼 */}
-                <S.Button onClick={onRegister}>
-                    <ContentTag text='등록' color='#fff' as='M' />
-                </S.Button>
-            </S.ButtonContainer>
-        </S.LocationContainer>
-    );
+            </S.LocationContainer>
+        );
+    } else if (as === 'subtitle') {
+        return (
+            <S.LocationContainer width={'100%'} justifyContent={'center'}>
+                <SubTitle text={location as string} lan='ENG' color='#fff' />
+            </S.LocationContainer>
+        );
+    }
 };
 export default Header;
