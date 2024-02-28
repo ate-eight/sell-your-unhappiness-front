6ec37d469f7b7a3dd7c0eb7d9c11faa66355dac5
bbung <tjd123123@naver.com>
2024-02-28 17:04:42 +0900

    refactor: private router fix

--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -1,18 +1,15 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 interface Props {
     isLogin: boolean;
 }
 
 const PrivateRouter = ({ isLogin }: Props) => {
-    return isLogin ? (
-        <Outlet />
-    ) : (
-        <>
-            <h1>로그인이 필요한 페이지입니다.</h1>
-            <Link to={'/'}>로그인 페이지</Link>
-        </>
-    );
+    if (!isLogin) {
+        return <Navigate replace to={'/'} />;
+    }
+
+    return <Outlet />;
 };
 
 export default PrivateRouter;
