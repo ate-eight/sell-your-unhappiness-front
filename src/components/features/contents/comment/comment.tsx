db4b601ac4b65afe01ad7047255a6ca2600591ef
bbung <tjd123123@naver.com>
2024-03-12 15:53:12 +0900

    feat: comment add api add

--- src/components/features/contents/comment/comment.tsx
+++ src/components/features/contents/comment/comment.tsx
@@ -1,11 +1,17 @@
 import SubTitle from '@components/common/text/SubTitle';
 import { useCallback, useState } from 'react';
 
+import { fetchData } from '@/api';
 import Divider from '@/components/common/divider/divider';
 import Input from '@/components/common/Input/Input';
 import CommentContainer from '@/components/features/contents/detail/commentContainer';
 
 import * as S from '../style';
+
+interface ICommentAddResponse {
+    message: string;
+}
+
 export interface IButtonData {
     label: string;
     color: 'primary' | 'secondary';
@@ -21,8 +27,29 @@ const ContentContainer = () => {
     /**
      * react-query mutation으로 query-key update
      */
-    const handleCommentSubmit = () => {
+    const handleCommentSubmit = async () => {
         alert('댓글 저장');
+
+        if (!inputValue) return;
+
+        const response = await fetchData.post<ICommentAddResponse>(
+            '/v1/board-comment',
+            JSON.stringify({
+                // parentId: 0,
+                boardId: 1,
+                content: inputValue,
+            }),
+        );
+
+        if (response) {
+            const { data, common } = response.data;
+
+            if (common.success) {
+                console.log(data);
+            } else {
+                console.error(common.message);
+            }
+        }
     };
 
     return (
